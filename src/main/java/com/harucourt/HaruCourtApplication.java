69d7feccd2060d7238e4e54476b57c5090dee120
cabbage16 <qocn16@gmail.com>
2025-06-09 20:14:14 +0900

    Initial commit

---
+++ src/main/java/com/harucourt/HaruCourtApplication.java
@@ -0,0 +1,13 @@
+package com.harucourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruCourtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruCourtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harucourt/HaruCourtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harucourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruCourtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
