d8477046e6728bc4f995e8f7cc4109bbe904c850
cabbage16 <qocn16@gmail.com>
2025-07-15 01:37:34 +0900

    feat: UUID 기반 사용자 조회 기능 추가

--- src/main/java/com/harucourt/infrastructure/persistence/user/UserRepository.java
+++ src/main/java/com/harucourt/infrastructure/persistence/user/UserRepository.java
@@ -4,7 +4,10 @@ import com.harucourt.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    User findByUuid(UUID uuid);
 }
