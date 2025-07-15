6026ffcdac5580478d50221d026bdce6409db70d
cabbage16 <qocn16@gmail.com>
2025-07-16 03:33:47 +0900

    feat: 로그아웃 시 사용자 존재 여부 검증 추가
    - 사용자 존재 확인 후 토큰 삭제하도록 수정

--- src/main/java/com/harucourt/application/auth/LogOutService.java
+++ src/main/java/com/harucourt/application/auth/LogOutService.java
@@ -1,6 +1,9 @@
 package com.harucourt.application.auth;
 
+import com.harucourt.domain.user.domain.User;
+import com.harucourt.domain.user.exception.UserNotFoundException;
 import com.harucourt.infrastructure.persistence.auth.TokenRepository;
+import com.harucourt.infrastructure.persistence.user.UserRepository;
 import com.harucourt.shared.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,8 +13,12 @@ import org.springframework.stereotype.Service;
 public class LogOutService {
 
     private final TokenRepository tokenRepository;
+    private final UserRepository userRepository;
 
     public void execute(CustomUserDetails userDetails) {
-        tokenRepository.deleteById(userDetails.uuid());
+        User user = userRepository.findByUuid(userDetails.uuid())
+                        .orElseThrow(UserNotFoundException::new);
+
+        tokenRepository.deleteById(user.getUuid());
     }
 }
